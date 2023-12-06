c50f08e8e6ca6768391fbf57b10eec683cdcf52e
clementmwamba <clementmwamba23@gmail.com>
2023-12-06 12:37:53 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer() 
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+greet("Naureen")
+
+def greet_with_default(name = "programmer"):
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    return num1 + num2
+
+add(1,2)
+
+ #If the function is called with an argument that isn't a number, it should return null:
+  #const result = halve("two")
+  #=> nul
 
 def halve(number):
-    pass
+    if type(number) != int:
+        print("null")
+        return "null"
+    print(number / 2)
+    return number / 2
+
+halve(6)
